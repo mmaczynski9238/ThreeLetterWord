3a45e1a2d2e7240652bcca8df6952a7ce69a0aaa
student3 <student3@jh-mbair-158-17.d214.org>
2015-12-14 14:40:46 -0600

    Started Visual Parts - Storyboard

--- ThreeLettterWord/ViewController.swift
+++ ThreeLettterWord/ViewController.swift
@@ -10,16 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var currentLetterLabel: UILabel!
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func whenTapped(sender: UITapGestureRecognizer) {
+        
+        
+        
+        
+        
+        
     }
-
 
 }
 
