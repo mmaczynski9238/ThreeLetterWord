0188345de9a5da1183df9e083bde439dd0fb55a9
student3 <student3@jh-mbair-158-17.d214.org>
2015-12-15 14:43:57 -0600

    Working copy w/ no reset

--- ThreeLettterWord/ViewController.swift
+++ ThreeLettterWord/ViewController.swift
@@ -15,18 +15,55 @@ class ViewController: UIViewController {
     @IBOutlet weak var label2: UILabel!
     @IBOutlet weak var label3: UILabel!
     
+    let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    var counter = 0
+    var currentLetter: Character!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    getCurrentLetter()
+    
     }
 
     @IBAction func whenTapped(sender: UITapGestureRecognizer) {
+    
+        let selectedPoint = sender.locationInView(self.view)
+        print(selectedPoint)
+        if CGRectContainsPoint(label1.frame, selectedPoint)
         
+        {
+          label1.text = String(currentLetter)
+        }
+       
+        if CGRectContainsPoint(label2.frame, selectedPoint)
+            
+        {
+            label2.text = String(currentLetter)
+        }
         
-        
-        
-        
-        
+        if CGRectContainsPoint(label3.frame, selectedPoint)
+            
+        {
+            label3.text = String(currentLetter)
+        }
+        counter++
+        getCurrentLetter()
+
+    
     }
+        func getCurrentLetter()
+        {
+            let myRange = Range<String.Index>(start: letters.startIndex.advancedBy(counter), end: letters.startIndex.advancedBy(counter+1))
+            currentLetterLabel.text = letters.substringWithRange(myRange)
+            currentLetter = Character   (letters.substringWithRange(myRange))
+            
+         
+        }
+        
+        
+        
+        
+    
 
 }
 
